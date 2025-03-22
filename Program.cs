7368933569fedc5e1fd11ba95d17971a8bb1c690
ThiagoRibeiro16 <thiago.escola321@gmail.com>
2025-03-22 14:32:59 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+string nome;
+string sobrenome;
+
+Console.Write($"{"digite seu nome".PadRight(20, '.')}: ");
+nome = Console.ReadLine()!;
+
+Console.Write($"{"digite seu sobrenome".PadRight(20, '.')}: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"seu nome é: {nome} {sobrenome}");
+
+Console.Write($"seu nome catálogo é: {sobrenome.ToUpper()}, {nome}");
